5e6ba00da7c631199c858c0f34571b8e2d637a0e
Maitryee-Choudhary <maitryeechudhary0715@gmail.com>
2023-07-28 15:17:50 +0530

    profile page

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,2 +1,23 @@
 
+import { getDataFromToken } from "@/helpers/getDataFromToken";
 
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import {connect} from "@/dbconfig/dbconfig";
+
+connect()
+
+export async function GET(request:NextRequest){
+    try{
+        const userID = await getDataFromToken(request);
+        const user = await User.findOne({_id: userID}).select("-password");
+        //console.log(user);
+        return NextResponse.json({
+            message: 'User found',
+            success: true,
+            data: user
+        });
+    }catch(error:any){
+        return NextResponse.json({error:error.message},{status:500});
+    }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,10 +2,13 @@
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import toast, {Toaster} from "react-hot-toast";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function Profile() {
 
     const router = useRouter();
+    const [userData, setUserData] = useState('nothing');
 
     const logout = async () => {
        try{
@@ -18,11 +21,21 @@ export default function Profile() {
        }
     }
 
+    const getUserDetails = async () => {
+       const res = await axios.get('/api/users/me');
+       console.log(res.data);
+       setUserData(res.data.data._id);
+
+    }
+
     return(
         <div>
             <h1>Profile</h1>
             <hr />
             <p>Profile Page</p>
+            <h2>{userData === 'nothing' ? "Nothing" : <Link href={`/profile/${userData}`}>View User Details {userData}</Link>
+            }</h2>
+            <button onClick={getUserDetails}>Get User Details</button>
             <button onClick={logout}>Logout</button>
             <Toaster />
         </div>
